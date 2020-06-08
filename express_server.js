0007719bf8ac4ddbc1704d39a1a4f0d7d1b8400d
Trevor Thomas <thomastrevor1@gmail.com>
2020-06-08 20:35:05 +0000

    installed express and ejs

--- express_server.js
+++ express_server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 3001; // default port 8080
 
+
+//set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -15,6 +21,12 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+console.log(`${PORT} is the magic port`);
