983b22954c0f127a75a165317dcc588f5cb08d0f
Trevor Thomas <thomastrevor1@gmail.com>
2020-06-09 01:04:00 +0000

    added redirect from the original longURL that was entered with the generated shortUrl, not linted, need to add edge cases, error handling

--- express_server.js
+++ express_server.js
@@ -9,6 +9,8 @@ app.set('view engine', 'ejs');
 
 // use res.render to load up an ejs view file
 
+let shortUrl = generateRandomString();
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -32,13 +34,28 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("We're working on figuring this out, hold on for like an hour or two");         // Respond with 'Ok' (we will replace this)
+  // console.log(req.body);
+
+  urlDatabase[shortUrl] = req.body.longURL;
+   console.log(urlDatabase); //logs the object with the newly added kvp (key:value pair)
+  res.redirect(`/urls/${shortUrl}`);
+  return;
+  // Log the POST request body to the console
+  // res.send("We're working on figuring this out, hold on for like an hour or two");         // Respond with 'Ok' (we will replace this)
 });
+
 
 app.get("/urls/:shortURL", (req, res) => {  
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]  };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+console.log("hello");
+  const longURL = urlDatabase[shortUrl]
+  console.log(longURL);
+  res.redirect(longURL);
+  return;
 });
 
 app.get("/hello", (req, res) => {
